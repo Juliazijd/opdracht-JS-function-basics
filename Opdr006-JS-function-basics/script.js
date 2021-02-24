37cf00152b1a4b13a3ab955e113eaea0eb233393
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2021-02-24 11:38:31 +0100

    Add files via upload

---
+++ Opdr006-JS-function-basics/script.js
@@ -0,0 +1,20 @@
+const makePizza = function() {
+    console.log("Step 1: Roll the dough");
+    console.log("Step 2: Evenly spread tomato sauce");
+    console.log("Step 3: Add veggies and of course CHEEESE");
+    console.log("Step 4: Put it in the oven for 15 minutes");
+}
+
+const makeSushi = function() {
+    console.log("Step 1: Cook the rice");
+    console.log("Step 2: Spread our rice over the seaweed leafs");
+    console.log("Step 3: Add veggies and fish");
+    console.log("Step 4: Roll up tight and cut them in even pieces");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
